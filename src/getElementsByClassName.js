508324febc4490bc05cba8648436b9209cc28531
forrestbthomas <forrestbthomas@gmail.com>
2014-05-06 11:27:28 -0700

    passed getElementsByCLassName super-stoked!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,32 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+var doc = document.body;
+var classArr = [];
+
+var recursiveFunc = function(node) {
+	if (node.classList !== undefined) {
+	  if (node.classList.contains(className)) {
+	    classArr.push(node)
+	  }
+	  if (node.hasChildNodes()) {
+	    for (var i = 0; i < node.childNodes.length; i++) {
+	      recursiveFunc(node.childNodes[i]);
+	    }
+	  }
+	} else {
+		if (node.className === className) {
+			classArr.push(node);
+		}
+	}
+return classArr;
 };
+recursiveFunc(doc);
+return classArr;
+    
+
+};
+
+
+
+
