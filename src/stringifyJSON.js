166c48180965036e0e19c29a9e1af79a041a6b97
forrestbthomas <forrestbthomas@gmail.com>
2014-05-06 09:46:29 -0700

    first commit stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,20 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') obj = "'" + obj + "'";
+  if (typeof obj === 'number') obj += '';
+  if (typeof obj === 'boolean') obj += '';
+  if (typeof obj === 'function') obj = undefined;
+  if (Array.isArray(obj)) {
+  	for (var i = 0; i < obj.length; i++) {
+  		stringifyJSON(obj[i]);
+  	}
+  	return obj = "'[" + obj + "]'";
+  };
+  if (typeof obj === 'object') {
+  	for (var key in obj) {
+  		stringifyJSON(obj[key])
+  	}
+  }
+  
 };
