6c2ccc782bd98ae1aa87bd8c64a90eb555ed026e
forrestbthomas <forrestbthomas@gmail.com>
2014-05-06 10:59:41 -0700

    almost there stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,21 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-  if (typeof obj === 'string') obj = "'" + obj + "'";
-  if (typeof obj === 'number') obj += '';
-  if (typeof obj === 'boolean') obj += '';
-  if (typeof obj === 'function') obj = undefined;
-  if (Array.isArray(obj)) {
-  	for (var i = 0; i < obj.length; i++) {
-  		stringifyJSON(obj[i]);
-  	}
-  	return obj = "'[" + obj + "]'";
-  };
-  if (typeof obj === 'object') {
-  	for (var key in obj) {
-  		stringifyJSON(obj[key])
-  	}
-  }
-  
+// your code goes here
+	var narr = [];
+	var nobj;
+	if (typeof obj === 'string') obj = '"' + obj + '"';
+	if (typeof obj === 'number') obj += "";
+	if (typeof obj === 'boolean') obj += '';
+	if (obj === null) return obj += '';
+	if (Array.isArray(obj)) {
+		for (var i = 0; i < obj.length; i++) {
+			narr.push(stringifyJSON(obj[i]));
+		}
+		return narr = "[" + narr + "]";
+	}
+	if (typeof obj === 'object') {
+		var objLength = Object.getOwnPropertyNames(obj).length;
+		if (objLength > 0) {
+			for (var key in obj) {
+				if (obj[key] === undefined || typeof obj[key] === 'function') {
+					delete obj[key];
+				}
+			}
+			if (objLength === 1) {
+				for (var key in obj) {
+					nobj = stringifyJSON(key) + ":" + stringifyJSON(obj[key]);
+				}
+				return nobj = '{' + nobj + '}';
+			} else {
+				for (var key in obj) {
+					nobj += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+				}
+				console.log(nobj);
+				nobj = nobj.slice(9, nobj.length - 1);
+				return nobj = '{' + nobj + '}';
+			}
+		} else {
+			return '{}';
+		}
+	}
+
+	return obj;
+
 };
